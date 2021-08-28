bf1a46ffcc2d38e133931072e2e27404fdfa2189
kisspa <kisspa@gmail.com>
2021-08-28 15:28:20 +0900

    lol

--- src/main/java/com/tistory/hitomis/springbootjspboard/SpringbootjspboardApplication.java
+++ src/main/java/com/tistory/hitomis/springbootjspboard/SpringbootjspboardApplication.java
@@ -5,11 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-/**
- * war 배포를 위해 extends SpringBootServletInitializer 상속
- * SpringApplicationBuilder 메소드 구현
- */
-
 @SpringBootApplication
 public class SpringbootjspboardApplication {
 
